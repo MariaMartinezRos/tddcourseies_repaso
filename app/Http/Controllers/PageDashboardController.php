da0608d6f17b8f431b5b8fd08a778b3f39f88959
MariaMartinezRos <cibernuggets@gmail.com>
2025-02-13 11:58:53 +0100

    Optimiza la consulta de PageDashboardController para que, al obtener los cursos de un usuario, también cargue los videos con Eager Loading para evitar el problema N+1

--- app/Http/Controllers/PageDashboardController.php
+++ app/Http/Controllers/PageDashboardController.php
@@ -6,7 +6,8 @@ class PageDashboardController extends Controller
 {
     public function __invoke()
     {
-        $purchasedCourses = auth()->user()->purchasedCourses;
+        // Cargar los videos relacionados con lo s cursos en una sola consulta
+        $purchasedCourses = auth()->user()->purchasedCourses()->with('videos')->get();
 
         return view('pages.dashboard', compact('purchasedCourses'));
     }
